f2457593ca0d3cf519beb30f840f6dcc42863c70
Viktor Korolyuk <8334048+viktorKorolyuk@users.noreply.github.com>
2022-05-15 10:47:00 -0500

    Create itchAPI.js from https://static.itch.io/api.js

---
+++ gmu-visual-arcade/src/lib/ItchAPI.js
@@ -0,0 +1,91 @@
+/**
+ * @typedef GameDataOptions
+ * @property {string} user
+ * @property {string} game
+ * @property {(data) => void} onComplete
+ * @property {string} secret
+ */
+
+/**
+ * @typedef GameData
+ * @property {string} id
+ * @property {string} title
+ * @property {string} price
+ * @property {string?} original_price
+ * @property {{
+ *  amount: number,
+ *  price: string,
+ *  title: string,
+ *  id: number,
+ *  amount_remaining: number,
+ *  available: boolean
+ * }?} rewards
+ * @property {{
+ *  id: string,
+ *  end_date: string,
+ *  title: string,
+ *  rate: number
+ * }?} sale
+ */
+
+/**
+ * 
+ * @param {GameDataOptions} opts 
+ */
+export function getGameData(opts) {
+  var domain, url, xhr;
+  if (opts == null) {
+    opts = {};
+  }
+  domain = opts.domain || "itch.io";
+  if (!opts.user) {
+    throw new Error("Missing user");
+  }
+  if (!opts.game) {
+    throw new Error("Missing game");
+  }
+  url = "https://" + opts.user + "." + domain + "/" + opts.game + "/data.json";
+  if (opts.secret) {
+    url = url + "?secret=" + opts.secret;
+  }
+  xhr = new XMLHttpRequest();
+  xhr.open("GET", url);
+  xhr.addEventListener("readystatechange", (function (_this) {
+    return function (e) {
+      var game;
+      if (xhr.readyState !== 4) {
+        return;
+      }
+      game = JSON.parse(xhr.responseText);
+      return typeof opts.onComplete === "function" ? opts.onComplete(game) : void 0;
+    };
+  })(this));
+  return xhr.send();
+};
+
+export function attachBuyButton(el, opts) {
+  var domain, height, left, top, width;
+  if (opts == null) {
+    opts = {};
+  }
+  
+  domain = opts.domain || "itch.io";
+  width = opts.width || 680;
+  height = opts.height || 400;
+  top = (window.height - height) / 2;
+  left = (window.width - width) / 2;
+  if (!opts.user) {
+    throw new Error("Missing user");
+  }
+  if (!opts.game) {
+    throw new Error("Missing game");
+  }
+  return el.onclick = function () {
+    var w;
+    w = window.open("https://" + opts.user + "." + domain + "/" + opts.game + "/purchase?popup=1", "purchase", "scrollbars=1, resizable=no, width=" + width + ", height=" + height + ", top=" + top + ", left=" + left);
+    if (typeof w.focus === "function") {
+      w.focus();
+    }
+    return false;
+  };
+};
