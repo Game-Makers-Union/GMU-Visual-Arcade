e797584477c610088b956f4651d1a741e7ed7f5a
Viktor <8334048+viktorKorolyuk@users.noreply.github.com>
2022-08-22 17:54:05 -0400

    Flatten project

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import { HashRouter, Routes, Route } from 'react-router-dom';
+import NavigationBar from './components/NavigationBar';
 import LandingPage from './pages/LandingPage';
 
 const App = () => {
   return (
     <HashRouter basename='/'>
+
+      <NavigationBar />
       <Routes>
         <Route path='/' element={<LandingPage />} />
         {/* Add 404 handler page */}
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -5,8 +5,6 @@ import GameView from '../components/GameView';
 const LandingPage = () => {
   return (
     <div>
-      <NavigationBar />
-      <GameView url="https://thai-duong.itch.io/dave-the-duck" />
     </div>
   );
 }
